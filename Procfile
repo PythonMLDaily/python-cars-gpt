07e064fb6429dea530e08f98e8e97a7cbdeab62a
modestasv <info@modestasv.lt>
2024-01-23 15:25:18 +0200

    Added Flask on top of the application + prepared for deployment and added example test queries

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: cd app && python app.py
--- src/app.py
+++ src/app.py
@@ -1,9 +1,47 @@
+import os
+from waitress import serve
 from dotenv import load_dotenv
-
+from flask import Flask, request, jsonify
 import chatbot
+
+app = Flask(__name__)
 
 # Load the .env file to be able to use the secrets
 load_dotenv()
 
-# print(chatbot.make_ai_request("What cars do you have in stock?", '#2'))
-print(chatbot.make_ai_request("Which of them has heated seats?", '#2'))
+
+@app.route('/api/chat', methods=['POST'])
+def chat_api():
+    json_request = request.get_json()
+
+    if 'query' not in json_request:
+        return jsonify({
+            "error": {
+                "query": "Please provide a question via Query parameter"
+            }
+        }, 422)
+
+    if 'identifier' not in json_request:
+        return jsonify({
+            "error": {
+                "identifier": "Please provide an identifier via Identifier parameter"
+            }
+        }, 422)
+
+    question = json_request['query']
+    identifier = json_request['identifier']
+
+    return jsonify({
+        "data": {
+            "question": question,
+            "identifier": identifier,
+            "answer": chatbot.make_ai_request(question, identifier)
+        }
+    })
+
+
+if __name__ == '__main__':
+    if os.getenv("DEBUG"):
+        app.run(host='0.0.0.0', port=8080, use_reloader=True, debug=True)
+    else:
+        serve(app, host="0.0.0.0", port=8080)
